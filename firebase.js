c80407763bab27b78ea7c03eee6e011671ddfc84
Vidhu Kumar S.V <vidhu.kumar01@gmail.com>
2026-04-11 22:22:27 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCUDzMXoGN8_lHs9PihH_68hjSfE1IxPk4",
+  authDomain: "vk-36-9d21a.firebaseapp.com",
+  projectId: "vk-36-9d21a",
+  storageBucket: "vk-36-9d21a.firebasestorage.app",
+  messagingSenderId: "83904901211",
+  appId: "1:83904901211:web:02131b004620461b2ccf7e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
